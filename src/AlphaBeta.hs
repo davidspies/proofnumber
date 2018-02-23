164e1aeb756d6bb7891a7a1191e20eb37d609339
David Spies <david.spies@leapyear.io>
2018-02-23 14:58:17 -0800

    Simplify implify

--- src/AlphaBeta.hs
+++ src/AlphaBeta.hs
@@ -6,13 +6,20 @@ module AlphaBeta
 import Control.Monad (foldM)
 import Control.Monad.Except (ExceptT, runExceptT, throwError)
 import qualified Control.Monad.Except as Except
-import Control.Monad.State (StateT, runStateT)
-import qualified Control.Monad.State as State
 import Data.Maybe (fromJust)
 import Data.Proxy (Proxy(..))
 
 import Game
-    (Action, Game, GameValue, Next(..), Position, makeMove, next, utility)
+    ( Action
+    , Game
+    , GameValue
+    , Next(..)
+    , Position
+    , gameValue
+    , makeMove
+    , next
+    , utility
+    )
 import Player (PlayerMap)
 import qualified Player
 import Runnable (Runnable, runProxy)
@@ -21,51 +28,41 @@ import Strategy (Strategy(..))
 newtype OrderingRule g m = OrderingRule ([Action g] -> m [Action g])
 newtype AlphaBeta g m = AlphaBeta {mkOrderingRule :: forall s. m s (OrderingRule g (m s))}
 
-maxOn :: Ord b => (a -> b) -> a -> a -> a
-maxOn f x y
-  | f y > f x = y
-  | otherwise = x
+data Selection g = Selection (Maybe (Action g)) GameValue
+
+type PValues = PlayerMap GameValue
+
+startPValues :: PValues
+startPValues = Player.fromList
+  [ (Player.Left, gameValue Player.Left (-1))
+  , (Player.Right, gameValue Player.Right (-1))
+  ]
 
 instance (Game g, Runnable m) => Strategy (AlphaBeta g m) g where
-  decideMoves g AlphaBeta{mkOrderingRule} pos = case next g pos of
-    Options player acts -> runProxy $ \(Proxy :: Proxy t) -> do
-      orderingRule@(OrderingRule r) <- mkOrderingRule
-      ordered <- r acts
-      let
-        search :: Action g -> StateT (Maybe GameValue) (m t) GameValue
-        search act = do
-          gv <- State.get
-          let pm = Player.fromList [(player, gv), (Player.opposite player, Nothing)]
-          newValue <- State.lift $ posValue g orderingRule pm $ makeMove g pos act
-          State.modify (maxOn (fmap (utility player)) (Just newValue))
-          return newValue
-      (moveValues, fromJust -> maxv) <- runStateT (mapM search ordered) Nothing
-      return [act | (act, v) <- zip ordered moveValues, v == maxv]
-    End _            -> []
-
-infty :: Double
-infty = read "Infinity"
-
-type PValues = PlayerMap (Maybe GameValue)
+  decideMoves g AlphaBeta{mkOrderingRule} pos = runProxy $ \(Proxy :: Proxy t) -> do
+    orderingRule <- mkOrderingRule
+    Selection (fromJust -> act) _ <- posValue g orderingRule startPValues pos
+    return [act]
 
 posValue :: forall g m. (Monad m, Game g)
-  => g -> OrderingRule g m -> PValues -> Position g -> m GameValue
+  => g -> OrderingRule g m -> PValues -> Position g -> m (Selection g)
 posValue g (OrderingRule orderIt) = go
   where
+    go :: PValues -> Position g -> m (Selection g)
     go bnds0 pos = case next g pos of
       Options player acts -> do
+        let cutOff = bnds0 Player.! Player.opposite player
+            lowBound = bnds0 Player.! player
         ordered <- orderIt acts
         let
-          search :: PValues -> Action g -> ExceptT GameValue m PValues
-          search bnds act = do
-            result <- Except.lift $ go bnds $ makeMove g pos act
-            let newBnds = Player.insert player (Just result) bnds
+          search :: Selection g -> Action g -> ExceptT (Selection g) m (Selection g)
+          search cur@(Selection _ curBest) act = do
+            let newBnds = Player.insert player curBest bnds0
+            Selection _ result <- Except.lift $ go newBnds (makeMove g pos act)
+            let newResult = Selection (Just act) result
             if
-              | utility player result > maybe infty (utility player) (
-                    bnds Player.! Player.opposite player) ->
-                  throwError result
-              | utility player result > maybe (-infty) (utility player) (bnds Player.! player) ->
-                  return newBnds
-              | otherwise -> return bnds
-        either id (fromJust . (Player.! player)) <$> runExceptT (foldM search bnds0 ordered)
-      End v               -> return v
+              | utility player result >= utility player cutOff -> throwError newResult
+              | utility player result > utility player curBest -> return newResult
+              | otherwise                                      -> return cur
+        either id id <$> runExceptT (foldM search (Selection Nothing lowBound) ordered)
+      End v               -> return $ Selection Nothing v
